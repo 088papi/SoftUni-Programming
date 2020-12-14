9d9a8e0ac28fc0cfb9fda83b8e4c3bb8776d23e1
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-14 18:52:02 +0200

    Committing tasks.

---
+++ C#/C# Fundamentals/Basic Syntax, Conditional Statements & Loops/Lab Tasks/Task6_ForeignLanguages.cs
@@ -0,0 +1,29 @@
+﻿using System;
+//100/100
+namespace Task6_ForeignLanguages
+{
+    class Task6_ForeignLanguages
+    {
+        static void Main(string[] args)
+        {
+            string country = Console.ReadLine();
+
+            switch(country)
+            {
+                case "USA":
+                case "England":
+                    Console.WriteLine("English");
+                        break;
+                case "Argentina":
+                case "Spain":
+                case "Mexico":
+                    Console.WriteLine("Spanish");
+                        break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+
+            }
+        }
+    }
+}
