923f0b410ea14b3cfec2d8590707f70d5ce78286
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-14 17:35:33 +0200

    Committing Task1_StudentInformation.cs

---
+++ C#/C# Fundamentals/Basic Syntax, Conditional Statements & Loops/Lab Tasks/Task1_StudentInformation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab_Tasks
+{
+    class Task1_StudentInformation
+    {
+        static void Main(string[] args)
+        {
+
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            double grade = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}, Age: {age}, Grade: {grade:F2}");
+
+        }
+    }
+}
