1c9f60d2baea8088576eb69cef29dd100d5a6402
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-14 19:25:13 +0200

    Committing tasks.

---
+++ C#/C# Fundamentals/Basic Syntax, Conditional Statements & Loops/Lab Tasks/Task10_MultiplicationTable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+//100/100
+namespace Task9_MultiplicationTable
+{
+    class Task10_MultiplicationTable
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+
+             
+                Console.WriteLine("{0} X {1} = {2}", n,i, n*i);
+                
+              
+            }
+
+        }
+    }
+}
---
+++ C#/C# Fundamentals/Basic Syntax, Conditional Statements & Loops/Lab Tasks/Task11_MultiplicationTable2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task11_MultiplicationTable2
+{
+    class Task11_MultiplicationTable2
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int multiplier = int.Parse(Console.ReadLine());
+
+            if (multiplier > 10)
+            {
+                Console.WriteLine("{0} X {1} = {2}", n, multiplier, n * multiplier);
+            }
+            else
+            {
+                for (int i = multiplier; i <= 10; i++)
+                {
+                    Console.WriteLine("{0} X {1} = {2}", n, i, n * i);
+                }
+            }
+        }
+    }
+}
