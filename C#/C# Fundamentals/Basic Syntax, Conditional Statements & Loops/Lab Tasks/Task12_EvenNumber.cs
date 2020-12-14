f3554e07a8f5016fe3e201bd086f7470bc46287b
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-14 20:43:41 +0200

    Comitting Task12_EvenNumber.cs and Task13_DebugTheCode.cs

---
+++ C#/C# Fundamentals/Basic Syntax, Conditional Statements & Loops/Lab Tasks/Task12_EvenNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+//100/100
+namespace Task12_EvenNumber
+{
+    class Task12_EvenNumber
+    {
+        static void Main(string[] args)
+        {
+
+
+            int number = int.Parse(Console.ReadLine());
+
+            while (number % 2 != 0)
+            {
+
+                Console.WriteLine("Please write an even number.");
+
+                number = int.Parse(Console.ReadLine());
+     
+            }
+            Console.WriteLine("The number is: {0}", Math.Abs(number));
+
+        }
+    }
+}
+
---
+++ C#/C# Fundamentals/Basic Syntax, Conditional Statements & Loops/Lab Tasks/Task13_DebugTheCode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+//100/100
+namespace Task13_DebugTheCode
+{
+    class Task13_DebugTheCode
+    {
+        static void Main(string[] args)
+        {
+            var startDate = DateTime.ParseExact(Console.ReadLine(),
+     "d.M.yyyy", CultureInfo.InvariantCulture);
+
+            var endDate = DateTime.ParseExact(Console.ReadLine(),
+                "d.M.yyyy", CultureInfo.InvariantCulture);
+        
+            int holidaysCount = 0;
+
+
+            for (var date = startDate; date <= endDate; date = date.AddDays(1))
+            {
+                if (date.DayOfWeek == DayOfWeek.Saturday ||
+                    date.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    holidaysCount++;
+                }
+            }
+            Console.WriteLine(holidaysCount);
+        }
+
+    }
+}
+
