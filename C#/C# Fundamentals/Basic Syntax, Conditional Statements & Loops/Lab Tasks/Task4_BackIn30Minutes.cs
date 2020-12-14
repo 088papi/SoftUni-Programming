e7b55ec8b0585960fca315294a37baea630349c0
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-14 18:06:35 +0200

    Committing Task4_BackIn30Minutes.cs

---
+++ C#/C# Fundamentals/Basic Syntax, Conditional Statements & Loops/Lab Tasks/Task4_BackIn30Minutes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task3_BackIn30Minutes
+{
+    class Task3_BackIn30Minutes
+    {
+        static void Main(string[] args)
+        {
+
+
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine()) + 30;
+
+            if (minutes > 59)
+            {
+                hours += 1;
+                minutes -= 60;
+            }
+
+            if(hours > 23)
+            {
+                hours = 0;
+            }
+         
+            Console.WriteLine("{0}:{1:D2}", hours, minutes);
+
+
+
+        }
+    }
+}
