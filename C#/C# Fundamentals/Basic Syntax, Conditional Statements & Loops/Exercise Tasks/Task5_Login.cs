8475d6bb101d7a00e03e47a14f578e30528d60f4
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-15 10:48:12 +0200

    Committing Task5_Login.cs

---
+++ C#/C# Fundamentals/Basic Syntax, Conditional Statements & Loops/Exercise Tasks/Task5_Login.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task5_Login
+{
+    class Task5_Login
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+
+            string password = "";
+            int counter = 0;
+
+
+            for (int i = username.Length -1; i >= 0; i--)
+            {
+                password += username[i];
+            }
+
+            
+            while (counter < 4)
+            {
+                string input = Console.ReadLine();
+                if (input.Equals(password))
+                {
+                    Console.WriteLine("User {0} logged in.", username);
+                    return;
+                }
+                else if (counter < 3)
+                {
+                    Console.WriteLine("Incorrect password. Try again.");
+                }
+                counter++;
+            }
+            if (counter == 4)
+            {
+                Console.WriteLine("User {0} blocked!", username);
+            }
+        }
+        
+    }
+}
