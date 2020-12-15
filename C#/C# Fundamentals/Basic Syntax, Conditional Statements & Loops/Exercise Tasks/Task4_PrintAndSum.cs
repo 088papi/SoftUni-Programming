82b3e4fa00719b8fcc471756421dabea14345aca
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-15 10:13:50 +0200

    Committing tasks.

---
+++ C#/C# Fundamentals/Basic Syntax, Conditional Statements & Loops/Exercise Tasks/Task4_PrintAndSum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task4_PrintAndSum
+{
+    class Task4_PrintAndSum
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = firstNum; i <= secondNum; i++)
+            {
+                sum += i;
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Sum: " + sum);
+        }
+
+       
+    }
+}
