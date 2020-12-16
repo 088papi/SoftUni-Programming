8b86bb38cc973fc2fc5f1ed803c8243cdb322d6e
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-16 10:25:53 +0200

    Committing Task6_StrongNumber.cs

---
+++ C#/C# Fundamentals/Basic Syntax, Conditional Statements & Loops/Exercise Tasks/Task6_StrongNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+//100/100
+namespace Task6_StrongNumber
+{
+    class Task6_StrongNumber
+    {
+        static void Main(string[] args)
+        {
+            string inputNumber = Console.ReadLine();
+            int sum = 0;
+
+            for (int i = 0; i < inputNumber.Length; i++)
+            {
+                int digit = inputNumber[i] - 48;
+                int factoriel = 1;
+                for (int j = 1; j <= digit; j++)
+                {
+
+                    factoriel *= j;
+                }
+                sum += factoriel;
+            }
+            int num = int.Parse(inputNumber);
+            if (sum == num)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+
+        }
+    }
+}
