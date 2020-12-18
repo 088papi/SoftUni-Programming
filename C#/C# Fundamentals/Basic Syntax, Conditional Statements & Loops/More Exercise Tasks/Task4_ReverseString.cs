01088a1d5b8540a2ce10e270ce07a9f26ad15056
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-18 09:51:00 +0200

    Committing Task4_ReverseString.cs

---
+++ C#/C# Fundamentals/Basic Syntax, Conditional Statements & Loops/More Exercise Tasks/Task4_ReverseString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task4_ReverseString
+{
+    class Task4_ReverseString
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string reversed = "";
+
+            for (int i = input.Length - 1; i >= 0 ; i--)
+            {
+                reversed += input[i];
+            }
+
+            Console.WriteLine(reversed);
+        }
+    }
+}
