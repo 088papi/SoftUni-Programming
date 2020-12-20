d8a07ec6ddba63d50548501251851f77635c958c
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-20 10:38:13 +0200

    Committing Task8_TriangleOfNumbers.cs

---
+++ C#/C# Fundamentals/Basic Syntax, Conditional Statements & Loops/Exercise Tasks/Task8_TriangleOfNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task8_TriangleOfNumbers
+{
+    class Task8_TriangleOfNumbers
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            int counter = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(counter + " ");
+                }
+                counter++;
+                Console.WriteLine();
+            }
+        }
+    }
+}
