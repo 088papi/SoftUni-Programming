a100da92ca5697f39546df3ec5e4f7efc1bac10a
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-20 13:32:01 +0200

    Committing Task6_ConcatenateData.cs

---
+++ C#/C# Basics/First Steps in Coding/Lab Tasks/Task6_ConcatenateData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task6_ConcatenateData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
