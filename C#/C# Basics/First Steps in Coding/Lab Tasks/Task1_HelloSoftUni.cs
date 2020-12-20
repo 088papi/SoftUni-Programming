a7fc98a43d1c77b596ba9b986e3b6e20ac8f5cfe
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-20 11:57:27 +0200

    Committing Task1_HelloSoftUni.cs

---
+++ C#/C# Basics/First Steps in Coding/Lab Tasks/Task1_HelloSoftUni.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task1_HelloSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
