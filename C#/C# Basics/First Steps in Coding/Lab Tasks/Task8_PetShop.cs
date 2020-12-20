44d1bb53d5100348a2f98959f069522b2763ed63
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-20 17:15:20 +0200

    Committing tasks

---
+++ C#/C# Basics/First Steps in Coding/Lab Tasks/Task8_PetShop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task8_PetShop
+{
+    class Task8_PetShop
+    {
+        static void Main(string[] args)
+        {
+            int dogNumber = int.Parse(Console.ReadLine());
+            int otherAnimals = int.Parse(Console.ReadLine());
+
+            Console.WriteLine((dogNumber * 2.5) + (otherAnimals * 4) + " lv."); 
+
+        }
+    }
+}
---
+++ C#/C# Basics/First Steps in Coding/Lab Tasks/Task9_YardGardening.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task9_YardGardening
+{
+    class Task9_YardGardening
+    {
+        static void Main(string[] args)
+        {
+            double sqMeters = double.Parse(Console.ReadLine());
+
+            double totalPrice = sqMeters * 7.61;
+            double discount = totalPrice - (0.18 * totalPrice);
+
+            Console.WriteLine("The final price is: {0:F2} lv.", discount);
+            Console.WriteLine("The discount is: {0:F2} lv.", totalPrice-discount);
+
+
+        }
+    }
+}
