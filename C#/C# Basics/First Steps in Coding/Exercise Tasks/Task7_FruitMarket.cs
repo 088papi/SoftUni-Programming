063e5a842e20992db5f17a29a8dde49fc6e254e4
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-21 11:16:57 +0200

    Committing tasks

---
+++ C#/C# Basics/First Steps in Coding/Exercise Tasks/Task7_FruitMarket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task7_FruitMarket
+{
+    class Task7_FruitMarket
+    {
+        static void Main(string[] args)
+        {
+            double strawberryPrice = double.Parse(Console.ReadLine());
+            double bananas = double.Parse(Console.ReadLine());
+            double oranges = double.Parse(Console.ReadLine());
+            double raspberries = double.Parse(Console.ReadLine());
+            double strawberries = double.Parse(Console.ReadLine());
+
+
+            double raspberryPrice = strawberryPrice * 0.5;
+            double orangePrice = raspberryPrice - (0.40 * raspberryPrice);
+            double bananaPrice = raspberryPrice - (0.80 * raspberryPrice);
+
+
+
+            Console.WriteLine("{0:F2}",bananas * bananaPrice + strawberries * strawberryPrice + oranges * orangePrice + raspberries * raspberryPrice);
+        }
+    }
+}
---
+++ C#/C# Basics/First Steps in Coding/Exercise Tasks/Task8_FishTank.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task8_FishTank
+{
+    class Task8_FishTank
+    {
+        static void Main(string[] args)
+        {
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int h = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            int v = a * b * h; //cm3
+            double totalLitres = v * 0.001; // litres 
+            double percent2 = percent / 100;
+
+            double total = totalLitres * (1 - percent2);
+
+            Console.WriteLine(total);
+               
+
+
+
+
+
+        }
+    }
+}
