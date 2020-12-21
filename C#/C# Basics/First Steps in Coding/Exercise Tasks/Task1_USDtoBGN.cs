4160175931741d5912313a28446a004fadcd4425
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-21 10:32:27 +0200

    Committing tasks

---
+++ C#/C# Basics/First Steps in Coding/Exercise Tasks/Task1_USDtoBGN.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Task1_USDtoBGN
+{
+    class Task1_USDtoBGN
+    {
+        static void Main(string[] args)
+        {
+            double usd = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(usd * 1.79549);
+        }
+    }
+}
---
+++ C#/C# Basics/First Steps in Coding/Exercise Tasks/Task2_RadiansToDegrees.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task2_RadiansToDegrees
+{
+    class Task2_RadiansToDegrees
+    {
+        static void Main(string[] args)
+        {
+
+            double radians = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(Math.Round(radians * 180 / Math.PI));
+            //: градус = радиан * 180 / π
+        }
+    }
+}
