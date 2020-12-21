736f0d60c49f1684dc8c98cc1d8fd714a7aedb5d
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-21 10:33:05 +0200

    Committing tasks

---
+++ C#/C# Basics/First Steps in Coding/Exercise Tasks/Task3_DepositCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task3_DepositCalculator
+{
+    class Task3_DepositCalculator
+    {
+        static void Main(string[] args)
+        {
+            double deposit = double.Parse(Console.ReadLine());
+            int months = int.Parse(Console.ReadLine());
+            double annualPercent = double.Parse(Console.ReadLine());
+
+
+            double interest = deposit * annualPercent / 100;
+            double monthlyInterest = interest / 12;
+            double sum = deposit + (months * monthlyInterest);
+
+            Console.WriteLine(sum);
+         
+        }
+    }
+}
---
+++ C#/C# Basics/First Steps in Coding/Exercise Tasks/Task4_VacationBookList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task4_VacationBookList
+{
+    class Task4_VacationBookList
+    {
+        static void Main(string[] args)
+        {
+            int pageNumber = int.Parse(Console.ReadLine());
+            double pagesPerHour = double.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+
+            double hoursPerBook = pageNumber / pagesPerHour;
+
+            double hoursPerDay = hoursPerBook / days;
+            Console.WriteLine(hoursPerDay);
+        }
+    }
+}
