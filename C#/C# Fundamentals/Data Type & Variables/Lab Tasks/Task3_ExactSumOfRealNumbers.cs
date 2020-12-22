fe0c78664dbee780c3c667d708b71a9137bcabea
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-22 17:58:49 +0200

    Committing Task3_ExactSumOfRealNumbers.cs

---
+++ C#/C# Fundamentals/Data Type & Variables/Lab Tasks/Task3_ExactSumOfRealNumbers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task3_ExactSumOfRealNumbers
+{
+    class Task3_ExactSumOfRealNumbers
+    {
+        static void Main(string[] args)
+        {
+            int inputNum = int.Parse(Console.ReadLine());
+
+
+            decimal sum = 0;
+
+            for (int i = 0; i < inputNum; i++)
+            {
+                decimal number = decimal.Parse(Console.ReadLine());
+
+                sum += number;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
