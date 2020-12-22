34129be134c2de95d596028014b4369be92c5d57
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-22 18:44:21 +0200

    Committing Task6_ReversedChars.cs

---
+++ C#/C# Fundamentals/Data Type & Variables/Lab Tasks/Task6_ReversedChars.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task6_ReversedChars
+{
+    class Task6_ReversedChars
+    {
+        static void Main(string[] args)
+        {
+            
+
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            string c = Console.ReadLine();
+
+
+            Console.WriteLine(String.Join(" ",c,a,b ));
+        }
+    }
+}
