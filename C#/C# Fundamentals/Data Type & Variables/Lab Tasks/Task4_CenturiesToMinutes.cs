56626aedeb2b7843350153230f0483c4363a39f0
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-22 18:09:25 +0200

    Committing Task4_CenturiesToMinutes.cs

---
+++ C#/C# Fundamentals/Data Type & Variables/Lab Tasks/Task4_CenturiesToMinutes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task4_CenturiesToMinutes
+{
+    class Task4_CenturiesToMinutes
+    {
+        static void Main(string[] args)
+        {
+
+
+            int centuries = int.Parse(Console.ReadLine());
+            int years = centuries * 100;
+            int days = (int)(years * 365.2422);
+            int hours = 24 * days;
+            int minutes = 60 * hours;
+
+            Console.WriteLine("{0} centuries = {1} years = {2} days = {3} hours = {4} minutes", centuries, years, days, hours, minutes);
+
+
+
+
+
+        }
+    }
+}
