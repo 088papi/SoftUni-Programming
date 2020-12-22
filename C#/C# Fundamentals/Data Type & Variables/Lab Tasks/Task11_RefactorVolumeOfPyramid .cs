8a85fa45b195ee8473c251f39973420c35f3212a
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-22 21:27:00 +0200

    Committing tasks

---
+++ C#/C# Fundamentals/Data Type & Variables/Lab Tasks/Task11_RefactorVolumeOfPyramid .cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task11_RefactorVolumeOfPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+          
+            double length = double.Parse(Console.ReadLine());
+            
+            double width = double.Parse(Console.ReadLine());
+            
+            double height = double.Parse(Console.ReadLine());
+            Console.Write("Heigth: ");
+            Console.Write("Length: ");
+            Console.Write("Width: ");
+       
+
+            double V = (length + width + height) / 3;
+            
+            Console.Write($"Pyramid Volume: {V:f2}");
+
+        }
+    }
+}
---
+++ C#/C# Fundamentals/Data Type & Variables/Lab Tasks/Task12_1RefactorSpecialNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task12_1RefactorSpecialNumbers
+{
+    class Task12_1RefactorSpecialNumbers
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int prime = 0;
+            bool isPrime = false;
+            for (int i = 1; i <= number; i++)
+            {
+                prime = i;
+                while (i > 0)
+                {
+                    sum += i % 10;
+                    i = i / 10;
+                }
+                isPrime = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine("{0} -> {1}", prime, isPrime);
+                sum = 0;
+                i = prime;
+            }
+        }
+        }
+}
