dc301a2c0083600b8d3ff330158163311cce4916
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-22 18:46:56 +0200

    Committing Task7_ConcatNames.cs

---
+++ C#/C# Fundamentals/Data Type & Variables/Lab Tasks/Task7_ConcatNames.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task7_ConcatNames
+{
+    class Task7_ConcatNames
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            string delimeter = Console.ReadLine();
+
+            Console.WriteLine(String.Join(delimeter, firstName,lastName));
+        
+        }
+    }
+}
