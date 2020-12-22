5c063c66fa95deec34f1d893158b77dc26c8bf02
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-22 19:28:04 +0200

    Committing Task10_LowerOrUpper.cs

---
+++ C#/C# Fundamentals/Data Type & Variables/Lab Tasks/Task10_LowerOrUpper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task10_LowerOrUpper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
