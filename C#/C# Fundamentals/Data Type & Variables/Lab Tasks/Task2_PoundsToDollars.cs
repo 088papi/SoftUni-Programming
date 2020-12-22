13b0ff2ead2ff3731c68a3556e1e33f974fb5532
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-22 17:42:17 +0200

    Committing Task2_PoundsToDollars.cs

---
+++ C#/C# Fundamentals/Data Type & Variables/Lab Tasks/Task2_PoundsToDollars.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task2_PoundsToDollars
+{
+    class Task2_PoundsToDollars
+    {
+        static void Main(string[] args)
+        {
+            double pounds  = double.Parse(Console.ReadLine());
+            double dollars = pounds * 1.31;
+
+            Console.WriteLine($"{dollars:f3}");
+        }
+    }
+}
