f3fe2e3d471a64b725d6985a1a2a106ad79c3663
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-23 11:39:51 +0200

    Committing Task8_BeerKegs.cs

---
+++ C#/C# Fundamentals/Data Type & Variables/Exercise Tasks/Task8_BeerKegs.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task8_BeerKegs
+{
+    class Task8_BeerKegs
+    {
+        static void Main(string[] args)
+        {
+
+            byte inputTimes = byte.Parse(Console.ReadLine());
+            double maxVolume = 0;
+            string winner = "";
+
+
+            for (int i = 0; i < inputTimes; i++)
+            {
+                string model = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+
+                double volume = Math.PI * radius * radius * height;
+
+                if(volume > maxVolume)
+                {
+                    maxVolume = volume;
+                    winner = model;
+                }
+
+            }
+
+            Console.WriteLine(winner);
+
+
+
+        }
+    }
+}
