87bccd4ddfcd37e8e7b10af63540e64407dffe3e
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-23 09:39:26 +0200

    Committing Task1_IntegerOperations.cs

---
+++ C#/C# Fundamentals/Data Type & Variables/Exercise Tasks/Task1_IntegerOperations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task1_IntegerOperations
+{
+    class Task1_IntegerOperations
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine(((a + b) / c) * d);
+
+        }
+    }
+}
