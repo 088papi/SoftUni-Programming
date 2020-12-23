a576f63afdec8503526333c92eb6faff648436bb
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-23 11:01:19 +0200

    Committing Task7_WaterOverflow.cs

---
+++ C#/C# Fundamentals/Data Type & Variables/Exercise Tasks/Task7_WaterOverflow.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task7_WaterOverflow
+{
+    class Task7_WaterOverflow
+    {
+        static void Main(string[] args)
+        {
+            short capacity = 255;
+
+            short sum = 0;
+
+            byte n = byte.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                short waterToAdd = short.Parse(Console.ReadLine());
+
+                sum += waterToAdd;
+
+                if (sum > capacity)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    sum -= waterToAdd;
+                }
+               
+                 
+               
+            
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
