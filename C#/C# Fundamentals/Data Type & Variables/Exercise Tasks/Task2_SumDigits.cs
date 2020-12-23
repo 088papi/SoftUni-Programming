d5cea220b773c12f40cc9a996636bb90857f7d79
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-23 10:15:59 +0200

    Committing Task2_SumDigits.cs

---
+++ C#/C# Fundamentals/Data Type & Variables/Exercise Tasks/Task2_SumDigits.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task2_SumDigits
+{
+    class Task2_SumDigits
+    {
+        static void Main(string[] args)
+        {
+            String num = Console.ReadLine();
+            int sum = 0;
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                int currentNum = (int)num[i] - 48;
+
+                sum += currentNum;
+
+            }
+
+          
+
+            Console.WriteLine(sum);
+           
+        }
+    }
+}
