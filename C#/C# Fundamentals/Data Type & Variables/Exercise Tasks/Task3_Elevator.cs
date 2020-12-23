131e2345f3726f59945fd13bf6c1d00476f57039
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-23 10:22:12 +0200

    Committing Task3_Elevator.cs

---
+++ C#/C# Fundamentals/Data Type & Variables/Exercise Tasks/Task3_Elevator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task3_Elevator
+{
+    class Task3_Elevator
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+
+            int courses = (int)Math.Ceiling((double)numberOfPeople / capacity);
+            Console.WriteLine(courses);
+
+        }
+    }
+}
