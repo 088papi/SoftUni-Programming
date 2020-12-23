7fe1d3d004098ad7374e13200946c61d75151c4e
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-23 11:39:22 +0200

    Committing Task9_SpiceMustFlow.cs

---
+++ C#/C# Fundamentals/Data Type & Variables/Exercise Tasks/Task9_SpiceMustFlow.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Task9_SpiceMustFlow
+{
+    class Task9_SpiceMustFlow
+    {
+        static void Main(string[] args)
+        {
+            int startingYield = int.Parse(Console.ReadLine());
+            byte dayCounter = 0;
+            int totalSpice = 0;
+            while (startingYield >= 100)
+            {
+                totalSpice += startingYield;
+
+
+                if (totalSpice >= 26) {
+                    totalSpice -= 26;
+                }
+
+
+            
+               
+
+                startingYield -= 10;
+                dayCounter++;
+          
+            
+            }
+
+            if (totalSpice >= 26)
+            {
+                totalSpice -= 26;
+            }
+
+
+            Console.WriteLine(dayCounter);
+            Console.WriteLine(totalSpice);
+
+        }
+    }
+}
+
