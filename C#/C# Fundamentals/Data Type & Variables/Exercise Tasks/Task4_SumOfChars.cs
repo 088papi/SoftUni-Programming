b03476d032a759b74086317c02977f161382f10a
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-23 10:26:24 +0200

    Committing Task4_SumOfChars.cs

---
+++ C#/C# Fundamentals/Data Type & Variables/Exercise Tasks/Task4_SumOfChars.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task4_SumOfChars
+{
+    class Task4_SumOfChars
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 0; i < lines; i++)
+            {
+                string c = Console.ReadLine();
+                sum += (int)c[0];
+            }
+
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
