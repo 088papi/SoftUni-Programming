2866c579a523c751256d1164508019d0896c64d5
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-24 07:36:12 +0200

    Committing Task10_PokeMon.cs

---
+++ C#/C# Fundamentals/Data Type & Variables/Exercise Tasks/Task10_PokeMon.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task10_PokeMon
+{
+    class Task10_PokeMon
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int M = int.Parse(Console.ReadLine());
+            int Y = int.Parse(Console.ReadLine());
+            byte counter = 0;
+
+            decimal halfN = N / 2.0m;
+            while (N >= M )
+            {
+                N -= M;
+                counter++;
+
+                if(N == halfN && Y != 0)
+                {
+                    if (N > Y && Y != 0)
+                    {
+                        N /= Y;
+                    }
+                }
+            }
+
+
+            Console.WriteLine(N);
+            Console.WriteLine(counter);
+
+        }
+    }
+}
