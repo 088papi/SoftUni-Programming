5cb14defea5543f42181dd51ebddf220d230e6a9
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-24 07:35:19 +0200

    Committing Task11_Snowballs.cs

---
+++ C#/C# Fundamentals/Data Type & Variables/Exercise Tasks/Task11_Snowballs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task11_Snowballs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfSnowballs = int.Parse(Console.ReadLine());
+          
+            long snowballValue = 0;
+            long maxValue = 0;
+            int maxSnow = 0;
+            int maxTime = 0;
+            int maxQuality = 0;
+
+            for (int i = 0; i < numberOfSnowballs; i++)
+            {
+                int snowballSnow = int.Parse(Console.ReadLine());
+                int snowballTime = int.Parse(Console.ReadLine());
+                int snowballQuality = int.Parse(Console.ReadLine());
+
+                snowballValue = (long) Math.Pow(snowballSnow / snowballTime, snowballQuality);
+            if(snowballValue > maxValue)
+                {
+                    maxValue = snowballValue;
+                    maxSnow = snowballSnow;
+                    maxTime = snowballTime;
+                    maxQuality = snowballQuality;
+                }
+            }
+
+            Console.WriteLine($"{maxSnow} : {maxTime} = {maxValue} ({maxQuality})");
+        }
+    }
+}
