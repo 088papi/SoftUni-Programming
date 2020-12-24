b9a6b8ffb66d2c4277cae9a84097bfd2b4b72a76
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-24 08:22:36 +0200

    Committing Task5_DecryptingMessages.cs

---
+++ C#/C# Fundamentals/Data Type & Variables/More Exercise Tasks/Task5_DecryptingMessages.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task5_DecryptingMessages
+{
+    class Task5_DecryptingMessages
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+            int lineNumber = int.Parse(Console.ReadLine());
+            string result = "";
+            for (int i = 0; i < lineNumber; i++)
+            {
+                string character = Console.ReadLine();
+                char c = character[0];
+                int charatcterToBeAdded = Convert.ToInt32(c) + key;
+                    result += (char)charatcterToBeAdded;
+
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
