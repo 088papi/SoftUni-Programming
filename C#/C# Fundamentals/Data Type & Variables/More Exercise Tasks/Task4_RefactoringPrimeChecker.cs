8eb760ca12fa0e4cfb1b6d2c21d61408c169e7f8
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-24 08:02:59 +0200

    Committing Task4_RefactoringPrimeChecker.cs

---
+++ C#/C# Fundamentals/Data Type & Variables/More Exercise Tasks/Task4_RefactoringPrimeChecker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task4_RefactoringPrimeChecker
+{
+    class Task4_RefactoringPrimeChecker
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            string prime = "true";
+            for (int i = 2; i <= number; i++)
+            {
+                
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                     
+                        prime = "false";
+                        break;
+                    } 
+                    else
+                    {
+                        prime = "true";
+                    }
+                }
+                Console.WriteLine("{0} -> {1}", i, prime);
+            }
+
+        }
+    }
+}
