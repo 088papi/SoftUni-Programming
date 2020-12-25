30838d69e2eec08a3a38f06a23a140a89245dcba
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-25 09:10:11 +0200

    Committing Task2_FromLeftToRight.cs

---
+++ C#/C# Fundamentals/Data Type & Variables/More Exercise Tasks/Task2_FromLeftToRight.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Task2_FromLeftToRight
+{
+    class Task2_FromLeftToRight
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < lines; i++)
+            {
+                string numbers = Console.ReadLine();
+
+                string[] numbersAsArray = numbers.Split(" ");
+                int leftNum = int.Parse(numbersAsArray[0]);
+                int rightNum = int.Parse(numbersAsArray[1]);
+
+               int leftSum = 0;
+                int rightSum = 0;
+
+                if (leftNum > rightNum)
+                {
+                    while (leftNum != 0)
+                    {
+                        leftSum += leftNum % 10;
+                        leftNum /= 10;
+                       
+                    }
+                    Console.WriteLine(Math.Abs(leftSum));
+                }
+                else
+                {
+                    while (rightNum != 0)
+                    {
+                        rightSum += rightNum % 10;
+                        rightNum /= 10;
+                      
+                    }
+                    Console.WriteLine(Math.Abs(rightSum));
+                }
+
+
+                
+
+                      
+            }
+        }
+    }
+}
