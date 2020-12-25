f100a64960c4c46b7a88152c3c859d788d65c570
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-25 10:07:27 +0200

    Committing Task6_BalancedBrackets.cs

---
+++ C#/C# Fundamentals/Data Type & Variables/More Exercise Tasks/Task6_BalancedBrackets.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task6_BalancedBrackets
+{
+    class Task6_BalancedBrackets
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+
+            bool isNotBalanced = true;
+            byte countLeft = 0;
+            byte countRight = 0;
+            for (int i = 0; i < lines; i++)
+            {
+                string s = Console.ReadLine();
+
+                if(s.Equals("("))
+                {
+                    countLeft++;
+                }
+                else if(s.Equals(")"))
+                {
+                    countRight++;
+                }    
+            }
+                if(countLeft == countRight)
+            {
+                Console.WriteLine("Balanced");
+            }
+                else
+            {
+                Console.WriteLine("Unbalanced");
+            }
+        }
+    }
+}
