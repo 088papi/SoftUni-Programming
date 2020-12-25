01942e77abcf832e9adfa1fa741c00fed3579836
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-25 12:57:44 +0200

    Task2_PrintNumbersInReverseOrder.cs

---
+++ C#/C# Fundamentals/Arrays/Lab Tasks/Task2_PrintNumbersInReverseOrder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task2_PrintNumbersInReverseOrder
+{
+    class Task2_PrintNumbersInReverseOrder
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            string[] numbersAsStrings = new string[number];
+           
+            for (int i = 0; i < number; i++)
+            {
+                string num = Console.ReadLine();
+
+                numbersAsStrings[i] = num;
+            }
+
+            for (int i = numbersAsStrings.Length-1; i >= 0 ; i--)
+            {
+                Console.WriteLine(numbersAsStrings[i] + " ");
+            }
+
+
+        }
+    }
+}
