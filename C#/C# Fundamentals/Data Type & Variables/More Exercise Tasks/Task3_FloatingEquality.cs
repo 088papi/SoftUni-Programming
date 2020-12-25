034b8444b605be8120e7e88e222fcea85e03ffbb
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-25 09:50:55 +0200

    Committing Task3_FloatingEquality.cs

---
+++ C#/C# Fundamentals/Data Type & Variables/More Exercise Tasks/Task3_FloatingEquality.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task3_FloatingEquality
+{
+    class Task3_FloatingEquality
+    {
+        static void Main(string[] args)
+        {
+            Decimal a = Decimal.Parse(Console.ReadLine());
+            Decimal b = Decimal.Parse(Console.ReadLine());
+            Decimal epsilon = 0.000001m;
+
+            if ((a-b) > epsilon)
+            {
+                Console.WriteLine("False");
+            }
+            else
+            {
+                Console.WriteLine("True");
+            }
+        }
+    }
+}
