a539acd48366e81047d005b7c9faad98f65adf43
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-26 09:19:07 +0200

    Committing Task5_SumEvenNumbers.cs

---
+++ C#/C# Fundamentals/Arrays/Lab Tasks/Task5_SumEvenNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Task5_SumEvenNumbers
+{
+    class Task5_SumEvenNumbers
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    sum += numbers[i];
+                }
+            }
+
+
+            Console.WriteLine(sum);
+        }
+    }
+}
