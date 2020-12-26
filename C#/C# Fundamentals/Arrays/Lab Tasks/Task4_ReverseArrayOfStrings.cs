3ddac73fd9b4352bd8baf72e4f6f85fb36b74239
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-26 09:18:32 +0200

    Committing Task4_ReverseArrayOfStrings.cs

---
+++ C#/C# Fundamentals/Arrays/Lab Tasks/Task4_ReverseArrayOfStrings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task4_ReverseArrayOfStrings
+{
+    class Task4_ReverseArrayOfStrings
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = Console.ReadLine().Split(" ");
+
+            for (int i = strings.Length-1 ; i >= 0; i--)
+            {
+                Console.Write(strings[i] + " ");
+            }
+
+
+        }
+    }
+}
