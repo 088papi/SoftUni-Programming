fa77ef06400b6a456dfc8ddf1d1a0667b3b2619e
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-26 09:26:50 +0200

    Committing Task6_EvenAndOddSubtraction.cs

---
+++ C#/C# Fundamentals/Arrays/Lab Tasks/Task6_EvenAndOddSubtraction.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace Task6_EvenAndOddSubtraction
+{
+    class Task6_EvenAndOddSubtraction
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+
+
+
+            int oddSum = 0;
+            int evenSum = 0;
+
+        for (int i = 0; i < numbers.Length; i++)
+            {
+                if(numbers[i] % 2 == 0)
+                {
+                    evenSum += numbers[i];
+                }
+                else
+                {
+                    oddSum += numbers[i];
+                }
+
+            }
+
+            Console.WriteLine(evenSum-oddSum);
+        }
+
+
+        
+    }
+}
