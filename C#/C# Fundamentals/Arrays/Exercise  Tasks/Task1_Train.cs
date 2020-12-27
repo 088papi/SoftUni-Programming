b38bddc95c34e4bf8df34dd9d09590d73f9d066b
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-27 10:47:48 +0200

    Committing Task1_Train.cs

---
+++ C#/C# Fundamentals/Arrays/Exercise  Tasks/Task1_Train.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task1_Train
+{
+    class Task1_Train
+    {
+        static void Main(string[] args)
+        {
+            int wagons = int.Parse(Console.ReadLine());
+
+            int[] train = new int[wagons];
+            int allPeople = 0;
+            for (int i = 0; i < wagons; i++)
+            {
+
+                int people = int.Parse(Console.ReadLine());
+                train[i] = people;
+                allPeople += people;
+            }
+
+
+            Console.WriteLine(String.Join(" ", train));
+            Console.WriteLine(allPeople);
+            
+        }
+    }
+}
