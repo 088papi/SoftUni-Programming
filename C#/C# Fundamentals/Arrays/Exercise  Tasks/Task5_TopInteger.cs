8a183e1e3448919b941c021874c21b6878e1e8a2
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-28 08:11:53 +0200

    Committing Task5_TopInteger.cs

---
+++ C#/C# Fundamentals/Arrays/Exercise  Tasks/Task5_TopInteger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Task5_TopInteger
+{
+    class Task5_TopInteger
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+           
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool isTopInteger = true;
+                int currentNumber = arr[i];
+               
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if(currentNumber <= arr[j])
+                    {
+                        isTopInteger = false;
+                    }
+                }
+
+                if(isTopInteger == true)
+                {
+                    Console.Write(currentNumber + " ");
+                }
+            }
+        }
+    }
+}
