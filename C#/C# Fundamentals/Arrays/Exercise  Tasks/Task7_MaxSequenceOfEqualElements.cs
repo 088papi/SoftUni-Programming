84a17da974e2f57bdc262c4fd7c2a203e2491188
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-28 08:58:38 +0200

    Committing Task7_MaxSequenceOfEqualElements.cs

---
+++ C#/C# Fundamentals/Arrays/Exercise  Tasks/Task7_MaxSequenceOfEqualElements.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace Task7_MaxSequenceOfEqualElements
+{
+    class Task7_MaxSequenceOfEqualElements
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                 .Split()
+                 .Select(int.Parse)
+                 .ToArray();
+            int currentMaxCount = 0;
+          
+            int sequenceNum = 0;
+            int maxCount = 0;
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                int currentNum = arr[i];
+                int nextNum = arr[i + 1];
+
+                if(currentNum == nextNum)
+                {
+                    currentMaxCount++;
+                }
+                else
+                {
+                    currentMaxCount = 0;
+                }
+
+                if(currentMaxCount > maxCount)
+                {
+                    maxCount = currentMaxCount;
+                    sequenceNum = currentNum;
+                }
+
+            }
+
+            for (int i = 0; i <= maxCount; i++)
+            {
+                Console.Write(sequenceNum + " ");
+            }
+
+
+        }
+    }
+}
