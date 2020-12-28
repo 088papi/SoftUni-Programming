91ea3e39cb1bf1acce3fabf81e92f2dacacfec28
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2020-12-28 09:12:47 +0200

    Committing Task8_MagicSum.cs

---
+++ C#/C# Fundamentals/Arrays/Exercise  Tasks/Task8_MagicSum.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace Task8_MagicSum
+{
+    class Task8_MagicSum
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+
+            int magicSum = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < numbers.Length-1; i++)
+            {
+                int currentNum = numbers[i];
+
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    int nextNum = numbers[j];
+                  
+                    if (currentNum + nextNum == magicSum)
+                    {
+                        Console.WriteLine(currentNum + " " + nextNum);
+                    }
+                }
+                
+               
+
+               
+            }
+
+        }
+    }
+}
