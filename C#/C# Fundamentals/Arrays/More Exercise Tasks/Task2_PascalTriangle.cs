ff06a3392eaad547de0a8b8ba6d29a256475f10e
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-03 08:12:01 +0200

    Committing Task2_PascalTriangle.cs

---
+++ C#/C# Fundamentals/Arrays/More Exercise Tasks/Task2_PascalTriangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task2_PascalTriangle
+{
+    class Task2_PascalTriangle
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < rows; i++)
+            {
+                int number = 1;
+
+                for (int j = 0; j <= i ; j++)
+                {
+                    Console.Write($"{number} ");
+                    number = number * (i - j) / (j + 1);
+                }
+
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
