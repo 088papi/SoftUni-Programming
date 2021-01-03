48a66bfe0e1396a34ba3723ea41964b562122955
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-03 21:24:34 +0200

    Committing tasks.

---
+++ C#/C# Basics/Conditional Statements/Lab Tasks/Task2_GreaterNumber.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task2_GreaterNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ C#/C# Basics/Conditional Statements/Lab Tasks/Task4_Number100200.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task4_Number100200
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
