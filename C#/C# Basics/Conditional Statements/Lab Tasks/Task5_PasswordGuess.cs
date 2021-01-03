d2123596b54825ac81bfae1bbe046526311978fa
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-03 21:27:17 +0200

    Committing Task5_PasswordGuess.cs

---
+++ C#/C# Basics/Conditional Statements/Lab Tasks/Task5_PasswordGuess.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task5_PasswordGuess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
