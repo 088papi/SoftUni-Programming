f9bbe7a1edb7d793d75c45333aa61895d6136e67
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-03 21:16:52 +0200

    Committing Task1_Excellent Result.cs

---
+++ C#/C# Basics/Conditional Statements/Lab Tasks/Task1_Excellent Result.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task1_Excellent_Result
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
