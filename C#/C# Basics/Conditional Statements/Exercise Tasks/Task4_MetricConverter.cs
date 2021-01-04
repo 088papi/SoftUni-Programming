4a5e514b5f291cd5df8887b1dbcf076b2a08c45f
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-04 10:17:58 +0200

    Committing Task4_MetricConverter.cs

---
+++ C#/C# Basics/Conditional Statements/Exercise Tasks/Task4_MetricConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task4_MetricConverter
+{
+    class Task4_MetricConverter
+    {
+        static void Main(string[] args)
+        {
+            double givenNumber = double.Parse(Console.ReadLine());
+            string inputMetric = Console.ReadLine();
+            string outputMetric = Console.ReadLine();
+
+
+            if(inputMetric.Equals("mm"))
+            {
+                givenNumber = givenNumber / 1000;
+            }
+            else if(inputMetric.Equals("cm"))
+            {
+                givenNumber = givenNumber / 100;
+            }
+
+            if(outputMetric.Equals("mm"))
+            {
+                givenNumber = givenNumber * 1000;
+            } 
+            else if(outputMetric.Equals("cm"))
+            {
+                givenNumber = givenNumber * 100;
+            }
+
+
+            Console.WriteLine("{0:F3}",givenNumber);
+        }
+    }
+}
