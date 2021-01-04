31f570f179cb63ce8c08cdd6bb5b29c26c7491a1
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-04 09:49:07 +0200

    Committing tasks

---
+++ C#/C# Basics/Conditional Statements/Exercise Tasks/Task2_BonusScore.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Task2_BonusScore
+{
+    class Task2_BonusScore
+    {
+        static void Main(string[] args)
+        {
+            int startScore = int.Parse(Console.ReadLine());
+            double bonus = 0;
+            
+            if(startScore <= 100)
+            {
+                bonus += 5;
+            }
+            else if (startScore > 100 && startScore < 1000)
+            {
+                bonus += 0.2 * startScore;
+            }
+            else
+            {
+                bonus += 0.1 * startScore;
+            }
+
+            if (startScore % 2 == 0)
+            {
+                bonus += 1;
+            }
+            else if (startScore % 10 == 5)
+            {
+                bonus += 2;
+            }
+          
+            Console.WriteLine(bonus);
+            Console.WriteLine(startScore + bonus);
+            
+        }
+    }
+}
