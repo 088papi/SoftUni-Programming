995c1ce6094b26ababb9474ba3b5fe9800a6c19b
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-04 10:56:42 +0200

    Committing Task6_GodzillaVsKong.cs

---
+++ C#/C# Basics/Conditional Statements/Exercise Tasks/Task6_GodzillaVsKong.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task6_GodzillaVsKong
+{
+    class Task6_GodzillaVsKong
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int statists = int.Parse(Console.ReadLine());
+            double priceOneStatist = double.Parse(Console.ReadLine());
+
+            double totalPriceStatists = 0.0;
+
+            if(statists < 150)
+            {
+                totalPriceStatists = statists * priceOneStatist;
+            }
+            else
+            {
+                totalPriceStatists = statists * priceOneStatist - (0.1 * statists * priceOneStatist);
+            }
+
+            double decorPrice = budget * 0.1;
+
+            if(decorPrice + totalPriceStatists > budget)
+            {
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine("Wingard needs {0:F2} leva more.", (decorPrice + totalPriceStatists - budget));
+            }
+            else
+            {
+                Console.WriteLine("Action!");
+                Console.WriteLine("Wingard starts filming with {0:F2} leva left.", (budget - (decorPrice + totalPriceStatists)));
+            }
+        }
+    }
+}
