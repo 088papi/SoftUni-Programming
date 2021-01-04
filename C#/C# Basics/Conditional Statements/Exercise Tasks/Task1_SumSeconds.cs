6aff4d79a12058178702216100b23fb97f94af55
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-04 09:32:02 +0200

    Committing Task1_SumSeconds.cs

---
+++ C#/C# Basics/Conditional Statements/Exercise Tasks/Task1_SumSeconds.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task1_SumSeconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstLine = int.Parse(Console.ReadLine());
+            int secondLine = int.Parse(Console.ReadLine());
+            int thirdLine = int.Parse(Console.ReadLine());
+
+            int sum = firstLine + secondLine + thirdLine;
+            int seconds = sum % 60;
+            int minutes = sum / 60;
+
+           if (seconds < 10)
+            {
+                Console.WriteLine($"{minutes}:0{seconds}");
+            }
+           else
+            {
+                Console.WriteLine($"{minutes}:{seconds}");
+            }
+
+        }
+    }
+}
