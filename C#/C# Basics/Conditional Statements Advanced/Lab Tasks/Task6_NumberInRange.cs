9fcfe2139532a0162940e4a2f01834af9357df99
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-04 13:07:08 +0200

    Committing tasks

---
+++ C#/C# Basics/Conditional Statements Advanced/Lab Tasks/Task6_NumberInRange.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task6_NumberInRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
