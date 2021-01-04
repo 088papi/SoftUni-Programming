53463e11f4705cd283489e55d460869deafa949c
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-04 10:36:44 +0200

    Committing tasks

---
+++ C#/C# Basics/Conditional Statements/Exercise Tasks/Task5_TimePlus15Minutes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task5_TimePlus15Minutes
+{
+    class Task5_TimePlus15Minutes
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            minutes = minutes + 15;
+
+            if (minutes >= 60)
+            {
+                hours += 1;
+                minutes -= 60;
+            }
+
+            if (hours>= 24)
+            {
+                hours -= 24;
+            }
+
+
+            Console.WriteLine("{0}:{1:00}", hours, minutes);
+        }
+    }
+}
--- Java/Java Basics/Conditional Statements/Exercise Tasks/Ex05_TimePlus15.java
+++ Java/Java Basics/Conditional Statements/Exercise Tasks/Ex05_TimePlus15.java
@@ -1,15 +1,26 @@
 import java.util.Scanner;
 
-public class Ex05_TimePlus15 {
+public class TimePlus15 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int hour = Integer.parseInt(scanner.nextLine());
+        int minutes = Integer.parseInt(scanner.nextLine());
+        int afterFewMinutes = minutes + 15;
+        int newMinutes = afterFewMinutes % 60;
 
-
-
-
-
+        if (afterFewMinutes >= 60) {
+            hour = hour + 1;
         }
-
+        if (hour > 23) {
+            hour = 0;
+        }
+        if (afterFewMinutes < 60) {
+            System.out.printf("%d:%d", hour, afterFewMinutes);
+        } else if (newMinutes < 10) {
+            System.out.printf("%d:0%d", hour, newMinutes);
+        } else {
+            System.out.printf("%d:%d", hour, newMinutes);
+        }
     }
-
+}
