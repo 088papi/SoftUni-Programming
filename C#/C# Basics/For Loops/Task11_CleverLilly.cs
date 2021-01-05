cfd4a8fd35634b3bfdabd7e45985d9fc6aafc5d4
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-05 18:20:37 +0200

    Committing Task11_CleverLilly.cs

---
+++ C#/C# Basics/For Loops/Task11_CleverLilly.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task11_CleverLilly
+{
+    class Task11_CleverLilly
+    {
+        static void Main(string[] args)
+        {
+            int years = int.Parse(Console.ReadLine());
+            double washingMachinePrice = double.Parse(Console.ReadLine());
+            int oneToyPrice = int.Parse(Console.ReadLine());
+            int toysNumber = 0;
+            double money = 0.0;
+            int stolenMoney = 0;
+
+            for (int i = 1; i <= years; i++)
+            {
+                if(i % 2 != 0)
+                {
+                    toysNumber++;
+                }
+                else
+                {
+                    money += 5 * i; // ДОВЪРШИ ТУК
+                    stolenMoney++;
+                }
+            }
+
+            money -= stolenMoney;
+            money+= toysNumber * oneToyPrice;
+
+            if(money >= washingMachinePrice)
+            {
+                Console.WriteLine("Yes! {0:F2}", money-washingMachinePrice);
+            }
+            else
+            {
+                Console.WriteLine("No! {0:F2}", Math.Abs(money-washingMachinePrice));
+            }
+        }
+    }
+}
