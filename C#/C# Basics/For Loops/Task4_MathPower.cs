e76e4d3454df534d93ce190ea4fd8d26bd78dcb6
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-05 18:17:05 +0200

    Committing Task4_MathPower.cs

---
+++ C#/C# Basics/For Loops/Task4_MathPower.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task4_MathPower
+{
+    class Task4_MathPower
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= n; i+=2)
+            {
+                Console.WriteLine(Math.Pow(2,i));
+            }
+        }
+    }
+}
