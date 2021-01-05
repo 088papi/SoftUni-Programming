e2e582908440cbc25ff9a18aa7847e2f7108ff30
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-05 18:16:05 +0200

    committing Task2_NumbersNto1.cs

---
+++ C#/C# Basics/For Loops/Task2_NumbersNto1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task2_NumbersNto1
+{
+    class Task2_NumbersNto1
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+
+            for (int i = n; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
