a5a46ace48ddf55401c8aa678428cb703db25685
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-05 18:17:34 +0200

    Committing Task5_CharacterSequence.cs

---
+++ C#/C# Basics/For Loops/Task5_CharacterSequence.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task5_CharacterSequence
+{
+    class Task5_CharacterSequence
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.WriteLine(s[i]);
+            }
+        }
+    }
+}
