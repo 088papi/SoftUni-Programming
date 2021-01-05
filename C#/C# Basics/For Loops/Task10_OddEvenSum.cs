05b63345d4ce14431e9fd817a18f8caa9d07da82
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-05 18:20:12 +0200

    Committing Task10_OddEvenSum.cs

---
+++ C#/C# Basics/For Loops/Task10_OddEvenSum.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Task10_OddEvenSum
+{
+    class Task10_OddEvenSum
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int oddSum = 0;
+            int evenSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if(i % 2 == 0)
+                {
+                    evenSum += num;
+                }
+                else
+                {
+                    oddSum += num;
+                }
+            }
+
+            if (oddSum == evenSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = {0}", oddSum);
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = {0}", Math.Abs(oddSum - evenSum));
+            }
+
+        }
+    }
+}
