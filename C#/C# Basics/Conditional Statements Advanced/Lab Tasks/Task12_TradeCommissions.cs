eb0df5a5053cae1266b4c58a1908c7d0a00784e8
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-05 14:29:59 +0200

    Committing Task12_TradeCommissions.cs

---
+++ C#/C# Basics/Conditional Statements Advanced/Lab Tasks/Task12_TradeCommissions.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Task12_TradeCommissions
+{
+    class Task12_TradeCommissions
+    {
+        static void Main(string[] args)
+        {
+            string town = Console.ReadLine();
+            double sales = double.Parse(Console.ReadLine());
+            double commission = 0;
+            if (sales < 0)
+            {
+                Console.WriteLine("error");
+                return;
+            }
+
+            switch(town)
+            {
+                case "Sofia":
+                    if (sales <= 500)
+                    {
+                        commission = 5;
+                    }
+                    else if (sales <= 1000)
+                    {
+                        commission = 7;
+                    } 
+                    else if(sales <= 10000)
+                    {
+                        commission = 8;
+                    }
+                    else
+                    {
+                        commission = 12;
+                    }
+                    break;
+                case "Varna":
+                    if (sales <= 500)
+                    {
+                        commission = 4.5;
+                    }
+                    else if (sales <= 1000)
+                    {
+                        commission = 7.5;
+                    }
+                    else if (sales <= 10000)
+                    {
+                        commission = 10;
+                    }
+                    else
+                    {
+                        commission = 13;
+                    }
+                    break;
+                case "Plovdiv":
+                    if (sales <= 500)
+                    {
+                        commission = 5.5;
+                    }
+                    else if (sales <= 1000)
+                    {
+                        commission = 8;
+                    }
+                    else if (sales <= 10000)
+                    {
+                        commission = 12;
+                    }
+                    else
+                    {
+                        commission = 14.5;
+                    }
+                    break;
+
+
+                default:
+                    Console.WriteLine("error");
+                    return;
+                
+            }
+
+            Console.WriteLine("{0:F2}", ((commission / 100) * sales)); 
+
+
+        }
+    }
+}
