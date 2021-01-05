318f879c9588c1f11055b7005dd72ba78d1a02a2
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-05 18:15:27 +0200

    Committing Task1_NumbersFrom1To100.cs

---
+++ C#/C# Basics/For Loops/Task1_NumbersFrom1To100.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task1_NumbersFrom1To100
+{
+    class Task1_NumbersFrom1To100
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
