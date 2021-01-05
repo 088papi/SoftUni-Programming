989965adf67a85f4fad4f19fbe45831f1d9a86a7
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-05 18:18:12 +0200

    Committing Task6_SumVowels.cs

---
+++ C#/C# Basics/For Loops/Task6_SumVowels.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task6_SumVowels
+{
+    class Task6_SumVowels
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            int sum = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                switch(s[i])
+                {
+                    case 'a':
+                        sum += 1;
+                        break;
+                    case 'e':
+                        sum += 2;
+                        break;
+                    case 'i':
+                        sum += 3;
+                        break;
+                    case 'o':
+                        sum += 4;
+                        break;
+                    case 'u':
+                        sum += 5;
+                        break;
+                        
+                }
+
+               
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
