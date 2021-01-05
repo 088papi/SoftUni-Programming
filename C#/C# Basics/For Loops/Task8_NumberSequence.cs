8ffaba1101c3f756e6e0d96674a483f852e50f44
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-05 18:19:06 +0200

    Committing Task8_NumberSequence.cs

---
+++ C#/C# Basics/For Loops/Task8_NumberSequence.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task8_NumberSequence
+{
+    class Task8_NumberSequence
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+
+            int maxNum = 0;
+            int minNum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if(number > maxNum)
+                {
+                    maxNum = number;
+                }
+
+
+                if (number<minNum)
+                {
+                    minNum = number;
+                }
+            }
+
+            Console.WriteLine("Max number: {0}", maxNum);
+            Console.WriteLine("Min number: {0}", minNum);
+
+
+        }
+    }
+}
