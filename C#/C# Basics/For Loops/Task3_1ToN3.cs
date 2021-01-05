5072611d4e70fa51354462d9c4af0fbbe3bb4fab
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-05 18:16:35 +0200

    Committing Task3_1ToN3.cs

---
+++ C#/C# Basics/For Loops/Task3_1ToN3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task3_1ToN3
+{
+    class Task3_1ToN3
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < n; i+=3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
