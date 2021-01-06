d7597525a4e32972f491292cfaf60146ad6ac60a
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-06 11:21:53 +0200

    Committing Task7_MinNumber.cs

---
+++ C#/C# Basics/While Loops/Lab Tasks/Task7_MinNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task7_MinNumber
+{
+    class Task7_MinNumber
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            int min = int.MaxValue;
+
+            while (!s.Equals("Stop"))
+            {
+                int currentNum = int.Parse(s);
+
+                if (currentNum < min)
+                {
+                    min = currentNum;
+                }
+
+
+
+                s = Console.ReadLine();
+            }
+
+
+            Console.WriteLine(min);
+        }
+    }
+}
