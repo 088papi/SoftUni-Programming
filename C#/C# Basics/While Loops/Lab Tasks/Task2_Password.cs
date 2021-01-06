ca8110a7fea854d6be5c5e933f355d2e6dacd3da
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-06 10:14:13 +0200

    Committing Task2_Password.cs

---
+++ C#/C# Basics/While Loops/Lab Tasks/Task2_Password.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task2_Password
+{
+    class Task2_Password
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = Console.ReadLine();
+            string input = Console.ReadLine();
+            while(!input.Equals(password))
+            {
+
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Welcome " + username);
+        }
+    }
+}
