c6ead2c7270baa49fca82e430036c6fcce4967e7
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-06 09:57:57 +0200

    Committing Task6_Salary.cs

---
+++ C#/C# Basics/For Loops/Exercise Tasks/Task6_Salary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task6_Salary
+{
+    class Task6_Salary
+    {
+        static void Main(string[] args)
+        {
+            int openedTabs = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < openedTabs; i++)
+            {
+                string website = Console.ReadLine();
+
+                switch(website)
+                {
+                    case "Facebook":
+                        salary -= 150;
+                        break;
+                    case "Instagram":
+                        salary -= 100;
+                        break;
+                    case "Reddit":
+                        salary -= 50;
+                        break;
+
+                        
+                }
+                if (salary <= 0)
+                {
+                    Console.WriteLine("You have lost your salary");
+                }
+            }
+
+            Console.WriteLine(salary);
+        }
+    }
+}
