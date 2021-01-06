cfb7f76eb6bc435e382b0451b3118a345eea6ee0
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-06 10:43:10 +0200

    Committing Task4_Sequence 2kPlus1.cs

---
+++ C#/C# Basics/While Loops/Lab Tasks/Task4_Sequence 2kPlus1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task4_Sequence_2kPlus1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
