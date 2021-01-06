da6c6bfee7446b3b9af1928b9257ac803c3306e8
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-06 10:33:37 +0200

    Committing Task3_SumNumbers.cs

---
+++ C#/C# Basics/While Loops/Lab Tasks/Task3_SumNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task3_SumNumbers
+{
+    class Task3_SumNumbers
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            while(true)
+            {
+                int n = int.Parse(Console.ReadLine());
+
+                sum += n;
+                if (sum >= num)
+                {
+                    break;
+                }
+            }
+       
+            Console.WriteLine(sum);
+        }
+    }
+}
