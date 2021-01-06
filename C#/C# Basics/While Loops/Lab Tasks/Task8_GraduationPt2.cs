a4b9c1301f11a1114819bb72b4776e547cfaadb3
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-06 11:36:56 +0200

    Committing Task8_GraduationPt2.cs

---
+++ C#/C# Basics/While Loops/Lab Tasks/Task8_GraduationPt2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task8_GraduationPt2
+{
+    class Task8_GraduationPt2
+    {
+        static void Main(string[] args)
+        {
+            double totalGrade = 0;
+            string name = Console.ReadLine();
+            int counter = 0;
+            while (counter <= 12)
+            {
+
+                double currentGrade = double.Parse(Console.ReadLine());
+
+                if(currentGrade <= 4.00)
+                {
+                    Console.WriteLine($"{name} has been excluded at {counter} grade");
+                    return;
+                }
+                else
+                {
+                    totalGrade += currentGrade;
+                }
+                
+
+
+                counter++;
+            }
+            Console.WriteLine("{0} graduated. Average grade: {1:F2}", name, totalGrade/ 12 );
+        }
+    }
+}
