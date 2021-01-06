b3e750011cac988090f7811faf540ca40f8a06a7
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-06 09:07:37 +0200

    Committing Task2_HalfSumElement.cs

---
+++ C#/C# Basics/For Loops/Exercise Tasks/Task2_HalfSumElement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task2_HalfSumElement
+{
+    class Task2_HalfSumElement
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int max = int.MinValue;
+            int min = 0;
+
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                sum += num;
+
+                if (num > max)
+                {
+                    max = num;
+                }
+
+                
+
+                
+            }
+            int sumWithoutMaxNum = sum - max;
+            if (max == sumWithoutMaxNum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = " + max);
+            }
+            else
+            {
+                int diff = Math.Abs(max - sumWithoutMaxNum);
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = " + diff);
+            }
+        }
+    }
+}
