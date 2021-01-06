a807dc6cb4099592d825669e39fad16b91c8e5c4
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-06 11:20:03 +0200

    Committing Task6_MaxNumber.cs

---
+++ C#/C# Basics/While Loops/Lab Tasks/Task6_MaxNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task6_MaxNumber
+{
+    class Task6_MaxNumber
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            int max = int.MinValue;
+
+            while (!s.Equals("Stop"))
+            {
+                int currentNum = int.Parse(s);
+
+                if(currentNum > max)
+                {
+                    max = currentNum;
+                }
+
+
+
+                s = Console.ReadLine();
+            }
+
+
+            Console.WriteLine(max);
+        }
+    }
+}
