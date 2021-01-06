01b1ab44c3485080a160506dfb135331c82e270e
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-06 11:14:16 +0200

    Committing Task5_AccountBalance.cs

---
+++ C#/C# Basics/While Loops/Lab Tasks/Task5_AccountBalance.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Task5_AccountBalance
+{
+    class Task5_AccountBalance
+    {
+        static void Main(string[] args)
+        {
+
+            double total = 0.0;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if(input.Equals("NoMoreMoney"))
+                {
+                    break;
+                }
+
+                double money = double.Parse(input);
+                if(money < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+
+                Console.WriteLine("Increase: " + money);
+                total += money;
+
+            }
+
+
+            Console.WriteLine("Total: " + total);
+
+
+
+        }
+    }
+}
