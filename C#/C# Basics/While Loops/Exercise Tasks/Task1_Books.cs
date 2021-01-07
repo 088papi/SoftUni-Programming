c1f26e72cb9281dfb1bbbb5afdc29ee414f0dd80
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-07 10:35:49 +0200

    Committing Task1_Books.cs

---
+++ C#/C# Basics/While Loops/Exercise Tasks/Task1_Books.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task1_Books
+{
+    class Task1_Books
+    {
+        static void Main(string[] args)
+        {
+            string searchedBook = Console.ReadLine();
+            string input = Console.ReadLine();
+            byte counter = 0;
+
+            while(!input.Equals(searchedBook))
+            {
+                if(input.Equals("No More Books"))
+                {
+                    Console.WriteLine("The book you search is not here!");
+                    Console.WriteLine("You checked {0} books.", counter);
+                    return;
+                }
+
+
+                counter++;
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("You checked {0} books and found it.", counter);
+        }
+    }
+}
