c6532fda346b64771905a61a2695d8dd61b65cfa
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-07 07:40:51 +0200

    Committing Task3_Combinations.cs

---
+++ C#/C# Basics/Nested Loops/Lab Tasks/Task3_Combinations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task3_Combinations
+{
+    class Task3_Combinations
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int validCounter = 0;
+            for (int x1 = 0; x1 <= number; x1++)
+            {
+                for (int x2 = 0; x2 <=number; x2++)
+                {
+                    for (int x3 = 0; x3 <= number; x3++)
+                    {
+                        if (x1 + x2 + x3 == number)
+                        {
+                            validCounter++;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(validCounter);
+        }
+    }
+}
