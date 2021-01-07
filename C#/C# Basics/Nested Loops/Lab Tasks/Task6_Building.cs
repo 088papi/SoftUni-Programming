2a8439a132f6c99632834e2a0050e79aede1e89c
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-07 07:37:56 +0200

    Committing Task6_Building.cs

---
+++ C#/C# Basics/Nested Loops/Lab Tasks/Task6_Building.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task6_Building
+{
+    class Task6_Building
+    {
+        static void Main(string[] args)
+        {
+            int col = int.Parse(Console.ReadLine());
+            int rows = int.Parse(Console.ReadLine());
+
+            for (int i = col; i >= 1; i--)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    if(i % 2 == 0)
+                    {
+                        Console.Write("O" + i + j + " ");
+                    }
+                    else if (i == col)
+                    {
+                        Console.Write("L" + i + j + " ");
+                    }
+                    else
+                    {
+                        Console.Write("A" + i + j + " ");
+                    }
+
+                    
+                    
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
