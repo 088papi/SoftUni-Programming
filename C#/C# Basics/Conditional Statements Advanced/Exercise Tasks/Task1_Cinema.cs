e50fac9d194e1e7dd7cd76cee5066e217106a5fc
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-07 11:22:35 +0200

    Committing Task1_Cinema.cs

---
+++ C#/C# Basics/Conditional Statements Advanced/Exercise Tasks/Task1_Cinema.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task1_Cinema
+{
+    class Task1_Cinema
+    {
+        static void Main(string[] args)
+        {
+            string typeOfProjection = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int columns = int.Parse(Console.ReadLine());
+
+            double total = 0;
+            switch (typeOfProjection)
+            {
+                case "Premiere":
+                    total = rows * columns * 12.00;
+                    break;
+                case "Normal":
+                    total = rows * columns * 7.5;
+                    break;
+                case "Discount":
+                    total = rows * columns * 5;
+                    break;
+            }
+
+
+
+            Console.WriteLine("{0:F2}", total);
+        }
+    }
+}
