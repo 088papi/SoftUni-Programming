f054a3b17145c84d4def8e4d6ad095eec4522567
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-07 10:48:14 +0200

    Comitting Task2_ExamPreparation.cs

---
+++ C#/C# Basics/While Loops/Exercise Tasks/Task2_ExamPreparation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Task2_ExamPreparation
+{
+    class Task2_ExamPreparation
+    {
+        static void Main(string[] args)
+        {
+            int maxPoorGradeCounter = int.Parse(Console.ReadLine());
+            int poorGradeCounter = 0;
+            int taskCounter = 0;
+            double totalScore = 0;
+
+            string lastProblem = "";
+            string task = "";
+
+            while (true)
+            {
+                lastProblem = task;
+                 task = Console.ReadLine();
+                if(task.Equals("Enough"))
+                {
+                    break;
+                }
+                taskCounter++;
+
+                int grade = int.Parse(Console.ReadLine());
+                totalScore += grade;
+                if (grade <= 4)
+                {
+                    poorGradeCounter++;
+                }
+
+                if(poorGradeCounter == maxPoorGradeCounter)
+                {
+                    Console.WriteLine("You need a break, {0} poor grades.", maxPoorGradeCounter);
+                    return;
+                }
+
+            }
+
+
+            Console.WriteLine("Average score: {0:F2}", totalScore/taskCounter);
+            Console.WriteLine("Number of problems: {0}", taskCounter);
+            Console.WriteLine("Last problem: {0}",lastProblem );
+           
+
+        }
+    }
+}
