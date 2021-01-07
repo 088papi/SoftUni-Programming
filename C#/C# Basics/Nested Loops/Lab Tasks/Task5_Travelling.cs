d78519db4682c6c46f4f73b7f73501e6be821fff
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-07 07:39:49 +0200

    Committing Task5_Travelling.cs

---
+++ C#/C# Basics/Nested Loops/Lab Tasks/Task5_Travelling.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task5_Travelling
+{
+    class Task5_Travelling
+    {
+        static void Main(string[] args)
+        {
+            string country = Console.ReadLine();
+            double totalMoney = 0;
+
+
+            while (!country.Equals("End"))
+            {
+                double neededMoney = double.Parse(Console.ReadLine());
+
+                while(totalMoney< neededMoney)
+                {
+                    double money = double.Parse(Console.ReadLine());
+
+                    totalMoney += money;
+
+                }
+
+                if (totalMoney >= neededMoney)
+                {
+                    Console.WriteLine($"Going to {country}!");
+                    totalMoney = 0;
+                }
+
+                country = Console.ReadLine();
+            }
+        }
+    }
+}
