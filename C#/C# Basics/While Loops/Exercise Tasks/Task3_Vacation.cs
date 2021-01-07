21623b1223d1964c99cc26724ba60136bfef8d76
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-07 10:48:59 +0200

    Committing Task3_Vacation.cs

---
+++ C#/C# Basics/While Loops/Exercise Tasks/Task3_Vacation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Task3_Vacation
+{
+    class Task3_Vacation
+    {
+        static void Main(string[] args)
+        {
+            double neededMoney = double.Parse(Console.ReadLine());
+            double currentMoney = double.Parse(Console.ReadLine());
+            byte spendingCounter = 0;
+            byte dayCounter = 0;
+            
+            while (spendingCounter < 5 && currentMoney < neededMoney)
+            {
+                string action = Console.ReadLine();
+                double money = double.Parse(Console.ReadLine());
+
+                switch(action)
+                {
+                    case "spend":
+
+                        if(money >= currentMoney)
+                        {
+                            currentMoney = 0;
+                        }
+                        else
+                        {
+                            currentMoney -= money;
+                        }
+                     
+                        spendingCounter++;
+
+                        break;
+                    case "save":
+                        currentMoney += money;
+                        spendingCounter = 0;
+                        break;
+                }
+
+
+                dayCounter++;
+
+            }
+            if (spendingCounter == 5)
+            {
+                Console.WriteLine("You can't save the money.");
+                Console.WriteLine(spendingCounter);
+            }
+            else
+            {
+                Console.WriteLine("You saved the money for {0} days.", dayCounter);
+            }
+        }
+    }
+}
