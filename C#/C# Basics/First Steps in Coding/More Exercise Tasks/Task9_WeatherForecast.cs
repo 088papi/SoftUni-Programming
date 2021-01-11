2a28c59818e0a746d857751b0e70c4fcecac01fb
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-11 11:45:36 +0200

    Committing Task9_WeatherForecast.cs

---
+++ C#/C# Basics/First Steps in Coding/More Exercise Tasks/Task9_WeatherForecast.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task9_WeatherForecast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
