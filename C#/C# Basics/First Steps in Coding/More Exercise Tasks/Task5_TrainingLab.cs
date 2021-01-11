27ee70368f54eb2d77de42e79de32d499fe33905
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-11 10:58:42 +0200

    Committing Task5_TrainingLab.cs

---
+++ C#/C# Basics/First Steps in Coding/More Exercise Tasks/Task5_TrainingLab.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task5_TrainingLab
+{
+    class Task5_TrainingLab
+    {
+        static void Main(string[] args)
+        {
+            double w = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+
+            double firstSeats = Math.Floor((h * 100 - 100) / 70);
+            double secondSeats = Math.Floor(w * 100 / 120);
+
+            double totalSeats = firstSeats * secondSeats - 3 ;
+            Console.WriteLine(totalSeats);
+        }
+    }
+}
