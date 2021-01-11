38d90f2bae1d8800edb45cbcca4b51033816eb5c
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-11 08:27:18 +0200

    Committing Task1_TrapeziodArea.cs

---
+++ C#/C# Basics/First Steps in Coding/More Exercise Tasks/Task1_TrapeziodArea.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task1_TrapeziodArea
+{
+    class Task1_TrapeziodArea
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+
+            double S = (a + b) * h / 2;
+
+            Console.WriteLine("{0:F2}", S);
+        }
+    }
+}
