4dbaeffdbe4498c6c309f62d73ff91cc174bedd0
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-11 10:18:27 +0200

    Committing Task3_CelsiusToFahrenheit.cs

---
+++ C#/C# Basics/First Steps in Coding/More Exercise Tasks/Task3_CelsiusToFahrenheit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task3_CelsiusToFahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
