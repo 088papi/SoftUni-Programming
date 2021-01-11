4848c0130d21d33daa71efec6ea04919d10b1137
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-11 10:26:48 +0200

    Committing Task4_VegetableMarket.cs

---
+++ C#/C# Basics/First Steps in Coding/More Exercise Tasks/Task4_VegetableMarket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task4_VegetableMarket
+{
+    class Task4_VegetableMarket
+    {
+        static void Main(string[] args)
+        {
+
+            double priceVeggies = double.Parse(Console.ReadLine());
+            double priceFruits = double.Parse(Console.ReadLine());
+            double veggiesKgs = double.Parse(Console.ReadLine());
+            double fruitsKgs = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("{0:F2}", ((priceVeggies * veggiesKgs) / 1.94) + ((priceFruits * fruitsKgs) / 1.94));
+        }
+    }
+}
