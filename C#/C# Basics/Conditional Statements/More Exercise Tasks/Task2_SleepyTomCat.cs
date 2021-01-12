d46d59b31c8091ef4ce71ea578c2dc0890b1a376
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-12 10:48:24 +0200

    Committing Task2_SleepyTomCat.cs

---
+++ C#/C# Basics/Conditional Statements/More Exercise Tasks/Task2_SleepyTomCat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task2_SleepyTomCat
+{
+    class Task2_SleepyTomCat
+    {
+        static void Main(string[] args)
+        {
+
+
+            int daysOff = int.Parse(Console.ReadLine());
+            int workdays = 365 - daysOff;
+
+            int allMinutes = daysOff * 127 + workdays * 63;
+
+            if(allMinutes > 30000)
+            {
+                int diff = allMinutes - 30000;
+                Console.WriteLine("Tom will run away");
+                Console.WriteLine("{0} hours and {1} minutes more for play", diff/60, diff%60);
+            }
+            else
+            {
+                int diff = 30000 - allMinutes;
+                Console.WriteLine("Tom sleeps well");
+                Console.WriteLine("{0} hours and {1} minutes less for play", diff/60, diff % 60 );
+            }
+
+        }
+    }
+}
