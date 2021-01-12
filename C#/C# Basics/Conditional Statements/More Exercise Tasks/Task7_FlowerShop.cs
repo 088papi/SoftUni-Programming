31b103cfa0e63e0fcf7d5197cdc1c0f62dee124f
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-12 16:49:26 +0200

    Committing Task7_FlowerShop.cs

---
+++ C#/C# Basics/Conditional Statements/More Exercise Tasks/Task7_FlowerShop.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task7_FlowerShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
