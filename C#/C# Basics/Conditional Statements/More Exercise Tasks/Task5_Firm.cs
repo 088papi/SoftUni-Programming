809acf70335849753cdfa81155888830cb5f4f5a
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-12 11:38:26 +0200

    Committing Task5_Firm.cs

---
+++ C#/C# Basics/Conditional Statements/More Exercise Tasks/Task5_Firm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task5_Firm
+{
+    class Task5_Firm
+    {
+        static void Main(string[] args)
+        {
+            int neededHours = int.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+            int overtimeWorkers = int.Parse(Console.ReadLine());
+
+            double hours = (days * 8) - 0.10 * (days * 8);
+            double overtime = overtimeWorkers * (2 * days);
+
+            hours += overtime;
+
+            hours = Math.Floor(hours);
+
+            if(hours >= neededHours)
+            {
+                Console.WriteLine("Yes!{0} hours left.", hours-neededHours);
+            }
+            else
+            {
+                Console.WriteLine("Not enough time!{0} hours needed.", neededHours-hours);
+            }
+
+        }
+    }
+}
