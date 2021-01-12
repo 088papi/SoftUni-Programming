c691f04baebc7d6650a762eff9527c9914ef5817
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-12 11:04:23 +0200

    Committing Task3_Harvest.cs

---
+++ C#/C# Basics/Conditional Statements/More Exercise Tasks/Task3_Harvest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task3_Harvest
+{
+    class Task3_Harvest
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            int z = int.Parse(Console.ReadLine());
+            int workers = int.Parse(Console.ReadLine());
+
+            double vineyard = x * y;
+            double wine = 0.4 * vineyard / 2.5;
+
+            if(wine < z)
+            {
+                Console.WriteLine("It will be a tough winter! More {0} liters wine needed.", Math.Floor(z-wine));
+            }
+            else
+            {
+                Console.WriteLine("Good harvest this year! Total wine: {0} liters", Math.Floor(wine));
+                Console.WriteLine("{0} liters left -> {1} liters per person.", Math.Ceiling(wine-z), Math.Ceiling((wine-z)/workers));
+            }
+        }
+    }
+}
