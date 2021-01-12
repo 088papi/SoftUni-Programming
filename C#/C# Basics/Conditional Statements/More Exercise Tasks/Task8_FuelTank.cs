f6bb95173ce300e9c0171012c990cf89b914c393
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-12 17:02:07 +0200

    Committing Task8_FuelTank.cs

---
+++ C#/C# Basics/Conditional Statements/More Exercise Tasks/Task8_FuelTank.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task8_FuelTank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
