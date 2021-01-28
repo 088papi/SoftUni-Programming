36efd704ef133cff99ee5d719122def9337d300b
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-28 19:09:44 +0200

    Committing Task4_AppliedArithmetic.java

---
+++ Java/Java Advanced/Fuctional Programming/Exercise Tasks/Task4_AppliedArithmetic.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Task4_AppliedArithmetic {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int[] nums = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt).toArray();
+
+        String command = scanner.nextLine();
+
+        Function<int[], int[]> add = e -> Arrays.stream(e).map(num -> num++).toArray();
+        Function<int[], int[]> subtract = e -> Arrays.stream(e).map(num -> num--).toArray();
+        Function<int[], int[]> multiply = e -> Arrays.stream(e).map(num -> num * 2).toArray();
+
+        Consumer<int[]> printer = e -> Arrays.stream(e).forEach(a -> System.out.print(a + " "));
+
+        while (!command.equals("end")){
+            switch (command){
+                case "add":
+                    nums = add.apply(nums);
+                    break;
+                case "multiply":
+                    nums = multiply.apply(nums);
+                    break;
+                case "subtract":
+                    nums = subtract.apply(nums);
+                    break;
+                case "print":
+                    printer.accept(nums);
+                    System.out.println();
+                    break;
+            }
+
+
+            command =  scanner.nextLine();
+        }
+
+    }
+}
