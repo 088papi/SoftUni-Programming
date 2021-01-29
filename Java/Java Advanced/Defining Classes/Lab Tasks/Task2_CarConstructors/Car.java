972f16c8bd9c4a522fb4e03632488e48b38f75fa
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-01-29 23:00:50 +0200

    Committing a Task2_CarConstructors/Car.java and Task2_CarConstructors/Main.java

---
+++ Java/Java Advanced/Defining Classes/Lab Tasks/Task2_CarConstructors/Car.java
@@ -0,0 +1,37 @@
+public class Car {
+    String brand;
+    String model;
+    int horsePower;
+
+    public Car(String brand){
+        this.brand = brand;
+        this.model = "unknown";
+        this.horsePower = -1;
+    }
+
+    public Car(String brand, String model, int horsePower){
+        this.brand = brand;
+        this.model = model;
+        this.horsePower = horsePower;
+    }
+
+
+
+    public String getBrand(){
+        return this.brand;
+    }
+    public String getModel(){
+        return this.model;
+    }
+    public int getHorsePower(){
+        return this.horsePower;
+    }
+
+
+    @Override
+    public String toString(){
+        return String.format("The car is: %s %s - %d HP.", getBrand(), getModel(),getHorsePower());
+    }
+
+
+}
