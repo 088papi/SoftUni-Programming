b6b6aa60e835356085ec499f1c26674126e79d61
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-02-08 21:11:09 +0200

    Committing src for task 8

---
+++ Java/Java Advanced/Generics/Exercise Tasks/Task8_CustomListSorter/Main.java
@@ -0,0 +1,63 @@
+package Task8;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        CustomList<String> customList = new CustomList<>();
+
+        while (!input.equals("END")) {
+            String[] data = input.split("\\s+");
+            String command = data[0];
+
+            String element;
+            switch (command) {
+                case "Add":
+                    element = data[1];
+                    customList.add(element);
+                    break;
+                case "Remove":
+                    element = data[1];
+                    customList.remove(Integer.parseInt(element));
+                    break;
+                case "Contains":
+                    element = data[1];
+                    System.out.println(customList.contains(element));
+                    break;
+                case "Swap":
+                    element = data[1];
+                    int index2 = Integer.parseInt(data[2]);
+                    customList.swap(Integer.parseInt(element), index2);
+                    break;
+                case "Greater":
+                    element = data[1];
+                    System.out.println(customList.countGreater(element));
+                    break;
+                case "Max":
+                    System.out.println(customList.getMax());
+                    break;
+                case "Min":
+                    System.out.println(customList.getMin());
+                    break;
+                case "Sort":
+                    Sorter.sort(customList);
+                    break;
+
+                case "Print":
+                    System.out.println(customList.print().trim());
+                    break;
+
+
+            }
+
+
+            input = scanner.nextLine();
+        }
+
+
+    }
+
+}
---
+++ Java/Java Advanced/Generics/Exercise Tasks/Task8_CustomListSorter/Sorter.java
@@ -0,0 +1,18 @@
+package Task8;
+
+import java.util.Collections;
+
+public class Sorter{
+    public static  <T extends Comparable<T>> void sort(CustomList<T> values){
+
+        for (int i = 0; i < values.size(); i++) {
+            T currentEl = values.get(i);
+            for (int j = i + 1; j < values.size() ; j++) {
+                if (currentEl.compareTo(values.get(j)) > 0){
+                    values.swap(i,j);
+                }
+            }
+        }
+    }
+
+}
