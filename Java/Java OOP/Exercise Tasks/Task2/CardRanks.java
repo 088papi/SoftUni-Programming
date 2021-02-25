76d6c8b2177a63629235e57eba3cf804e0912b8c
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-02-25 21:58:45 +0200

    Committing src for task 2

---
+++ Java/Java OOP/Exercise Tasks/Task2/CardRanks.java
@@ -0,0 +1,19 @@
+package Task2;
+
+public enum CardRanks {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING
+
+
+}
---
+++ Java/Java OOP/Exercise Tasks/Task2/Main.java
@@ -0,0 +1,13 @@
+package Task2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        CardRanks[] values = CardRanks.values();
+        System.out.println("Card Ranks:");
+        for (CardRanks value : values) {
+            System.out.printf("Ordinal value: %d; Name value: %s%n", value.ordinal(), value.name());
+        }
+
+    }
+}
