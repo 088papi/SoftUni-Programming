06132e184a7a8f4cb9e9abb403b0b3e2fb89360b
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-03-02 20:59:44 +0200

    Committing src for task 4

---
+++ Java/Java OOP/Inheritance/Lab Tasks/Task4_RandomArrayList/arrayList/RandomArrayList.java
@@ -0,0 +1,14 @@
+package arrayList;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomArrayList<T> extends ArrayList<T> {
+
+    public T getRandomElement(){
+        int index  = ThreadLocalRandom.current().nextInt(super.size());
+    return remove(index);
+    }
+
+}
