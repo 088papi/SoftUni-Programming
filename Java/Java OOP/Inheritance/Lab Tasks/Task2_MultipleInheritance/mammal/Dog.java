d44841858d41ef39f38153d261100df17892190d
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-03-02 19:58:03 +0200

    Committing src for task 2 Multiple Inheritance

---
+++ Java/Java OOP/Inheritance/Lab Tasks/Task2_MultipleInheritance/mammal/Dog.java
@@ -0,0 +1,10 @@
+package mammal;
+
+import mammal.Animal;
+
+public class Dog extends Animal {
+
+    public void bark(){
+        System.out.println("barking...");
+    }
+}
---
+++ Java/Java OOP/Inheritance/Lab Tasks/Task2_MultipleInheritance/mammal/Main.java
@@ -0,0 +1,11 @@
+package mammal;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Puppy puppy = new Puppy();
+        puppy.weep();
+        puppy.bark();
+        puppy.eat();
+    }
+}
