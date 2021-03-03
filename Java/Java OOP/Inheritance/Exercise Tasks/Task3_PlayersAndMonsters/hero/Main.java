6ca769aadd7e6dc1dbb709ab95c3253cf2e43b94
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-03-03 23:06:56 +0200

    Committing src for task 3 heroes and monsters

---
+++ Java/Java OOP/Inheritance/Exercise Tasks/Task3_PlayersAndMonsters/hero/Main.java
@@ -0,0 +1,12 @@
+package hero;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Hero soulMaster = new SoulMaster("Pesho", 5);
+        System.out.println(soulMaster.toString());
+
+    }
+
+}
