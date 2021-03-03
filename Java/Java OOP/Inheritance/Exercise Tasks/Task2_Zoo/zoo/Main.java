8e6583ad4ca0e34eb5f363d0b6d8ba05a9b5a3e4
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-03-03 22:51:20 +0200

    Committing src for task 2 Zoo

---
+++ Java/Java OOP/Inheritance/Exercise Tasks/Task2_Zoo/zoo/Main.java
@@ -0,0 +1,8 @@
+package zoo;
+
+public class Main {
+    public static void main(String[] args) {
+        Mammal gorilla1 = new Gorilla("Pesho");
+        System.out.println(gorilla1.getName());
+    }
+}
