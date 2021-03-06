41f925471d7c7b27b80af75629c09d148699205f
Pavlin Orlovski <75259724+pavlinorlovski@users.noreply.github.com>
2021-03-06 20:30:51 +0200

    Committing src for task 4

---
+++ Java/Java OOP/Interfaces and Abstraction/Lab Tasks/Task4_SayHelloExtended/nationalities/Bulgarian.java
@@ -0,0 +1,13 @@
+package nationalities;
+
+public class Bulgarian extends BasePerson{
+
+    public Bulgarian(String name){
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Здравей";
+    }
+}
---
+++ Java/Java OOP/Interfaces and Abstraction/Lab Tasks/Task4_SayHelloExtended/nationalities/Chinese.java
@@ -0,0 +1,14 @@
+package nationalities;
+
+public class Chinese extends BasePerson {
+
+
+    public Chinese(String name){
+        super(name);
+    }
+
+    @Override
+    public String sayHello() {
+        return "Djydjybydjy";
+    }
+}
